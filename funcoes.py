24c326b9643a9e150e1581f5eb6bd173a98da3d6
adriarochamendes <adriarochamendes@gmail.com>
2026-04-30 13:24:40 -0300

    Questão 2: Guardar dado

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,5 @@
+#questao1
+
 import random
 
 def rolar_dados(qtd):
@@ -8,3 +10,13 @@ def rolar_dados(qtd):
         lista.append(dado)
     
     return lista
+
+#questao2
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dado = dados_rolados[dado_para_guardar]
+    dados_no_estoque.append(dado)
+    del dados_rolados[dado_para_guardar]
+
+    return [dados_rolados, dados_no_estoque]
+
